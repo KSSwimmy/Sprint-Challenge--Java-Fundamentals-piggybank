0cfa69c2a7c3c315a063f4066995dd923269e674
KimberlySwinton <kimberlyswinton@gmail.com>
2019-08-09 13:07:33 -0400

    done boilerplating time for the Main event

--- src/piggyBank/Nickel.java
+++ src/piggyBank/Nickel.java
@@ -2,5 +2,31 @@ package piggyBank;
 
 public class Nickel extends AbstractCoin 
 {
-	
+	private int nickel;
+    
+    public Nickel(int nickel)
+    {
+        this.nickel = nickel;
+      
+    }
+
+    public Nickel()
+
+    {
+        this.nickel = this.nickel + 1;
+    }
+
+
+    public double getValue()
+    {
+        return nickel * 0.05;
+    }
+
+
+
+    public String getName()
+    {
+        String plural = nickel > 1 ? "s":"";
+        return this.nickel + " Nickel" + plural;
+    }
 }
--- src/piggyBank/Quarter.java
+++ src/piggyBank/Quarter.java
@@ -2,5 +2,25 @@ package piggyBank;
 
 public class Quarter extends AbstractCoin 
 {
+	  private int quarter;
+    
+    public Quarter(int quarter)
+    {
+        this.quarter = quarter;
+    }
+
+    public Quarter()
+    {
+        this.quarter = this.quarter + 1;
+    }
 	
+    public double getValue(){
+        return quarter * 0.25;
+    }
+	
+    public String getName()
+    {
+        String plural = quarter > 1 ? "s":"";
+        return this.quarter + " Quarter" + plural;
+    }
 }
